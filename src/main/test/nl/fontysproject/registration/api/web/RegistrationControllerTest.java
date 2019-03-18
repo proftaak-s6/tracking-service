f869b11e9eebc3232117251f216b19d7e8faa368
Casper Pijnenburg <casper.pijnenburg@student.fontys.nl>
2019-03-18 14:23:19 +0100

    Added test for basic get request

--- src/main/test/nl/fontysproject/registration/api/web/RegistrationControllerTest.java
+++ src/main/test/nl/fontysproject/registration/api/web/RegistrationControllerTest.java
@@ -1,10 +1,14 @@
 package nl.fontysproject.registration.api.web;
 
+import nl.fontysproject.registration.api.model.Registration;
+import nl.fontysproject.registration.api.web.controller.RegistrationController;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.test.JerseyTest;
 import org.junit.BeforeClass;
+import org.junit.Test;
 
 import javax.ws.rs.core.Application;
+import javax.ws.rs.core.Response;
 
 import static org.junit.Assert.*;
 
@@ -13,5 +17,13 @@ public class RegistrationControllerTest extends JerseyTest {
     @Override
     protected Application configure() {
         return new ResourceConfig(RegistrationController.class);
+    }
+
+    @Test
+    public void getRegistrations_Returns200AndListOfRegistrations() {
+        Response response = target("registrations").request().get();
+
+        assertEquals(200, response.getStatus());
+        assertEquals(Registration[].class, response.readEntity(Registration[].class).getClass());
     }
 }
