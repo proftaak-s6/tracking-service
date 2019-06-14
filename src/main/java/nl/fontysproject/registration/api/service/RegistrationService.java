91f9d8b85ce767206e5a61ec9146bd3fc4233669
Casper Pijnenburg <casper.pijnenburg@student.fontys.nl>
2019-06-14 12:42:57 +0200

    Removed old project

--- src/main/java/nl/fontysproject/registration/api/service/RegistrationService.java
+++
@@ -1,9 +0,0 @@
-package nl.fontysproject.registration.api.service;
-
-import nl.fontysproject.registration.api.model.Registration;
-
-import java.util.List;
-
-public interface RegistrationService {
-    List<Registration> getAll();
-}
--- src/main/java/nl/fontysproject/registration/api/web/Api.java
+++
@@ -1,8 +0,0 @@
-package nl.fontysproject.registration.api.web;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class Api extends Application {
-}
